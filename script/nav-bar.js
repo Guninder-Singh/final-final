4af9c81de069885502986cde5ce5aad29d716b8d
NewKungFuKenny <125688305+Guninder-Singh@users.noreply.github.com>
2023-08-29 02:43:21 +0530

    Update nav-bar.js

--- script/nav-bar.js
+++ script/nav-bar.js
@@ -3,12 +3,11 @@ var services = document.getElementById('projects');
 
 
 achievements.addEventListener('click', function() {
-  $(achievements).toggleClass("active");
-  $(".parent:not(#achievements)").toggleClass("invisible");
+ $(achievements).toggleClass("active");
+ $(".parent:not(#achievements)").toggleClass("invisible");
 }, false);
 
 projects.addEventListener('click', function() {
-  $(projects).toggleClass("active");
-  $(".parent:not(#projects)").toggleClass("invisible");
+ $(projects).toggleClass("active");
+ $(".parent:not(#projects)").toggleClass("invisible");
 }, false);
-
