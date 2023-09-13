946627141573a56ec6743b176e96e8a7dc8b0a39
Ridhi Aggarwal <raggarwal1_be21@thapar.edu>
2023-09-13 10:07:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-/*=============== SHOW MENU ===============*/
 const showMenu = (toggleId, navId) => {
     const toggle = document.getElementById(toggleId);
     const nav = document.getElementById(navId);
@@ -9,14 +8,24 @@ const showMenu = (toggleId, navId) => {
         return;
     }
 
-    toggle.addEventListener('click', () => {
+    // Use a function to handle the click event
+    const toggleMenu = () => {
         // Toggle the 'show-menu' class on the nav element
         nav.classList.toggle('show-menu');
 
         // Optionally, toggle a class on the toggle button for styling
         toggle.classList.toggle('active'); // Add a class like 'active' for styling purposes
+    };
+
+    // Add a click event listener to the toggle button
+    toggle.addEventListener('click', toggleMenu);
+
+    // Optionally, you can close the menu when a menu item is clicked
+    const navLinks = nav.querySelectorAll('.nav__link'); // Adjust the selector accordingly
+
+    navLinks.forEach((link) => {
+        link.addEventListener('click', toggleMenu);
     });
 };
 
 showMenu('nav-toggle', 'nav-menu');
-
