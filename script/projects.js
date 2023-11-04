cb747f3117018ea8233ebe2adaf72faef5431b8d
Ridhi Aggarwal <127647709+Nox1916@users.noreply.github.com>
2023-11-05 04:16:04 +0530

    Update projects.js

--- script/projects.js
+++ script/projects.js
@@ -1,19 +1,10 @@
-/* 
-Inspiration: 
-1. https://dribbble.com/shots/20496031-Discover-rare-Indian-treasure-NFTs-Landing-Page
-*/
-
-var swiper = new Swiper(".mySwiper", {
-    spaceBetween: 30,
-    centeredSlides: true,
-    loop: true,
-    autoplay: {
-      delay: 4000,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
+$(".custom-carousel").owlCarousel({
+  autoWidth: true,
+  loop: true
+});
+$(document).ready(function () {
+  $(".custom-carousel .item").click(function () {
+    $(".custom-carousel .item").not($(this)).removeClass("active");
+    $(this).toggleClass("active");
   });
-  
+});
