cd92f6e173035f4176765596585064d53881b2b3
Ridhi Aggarwal <127647709+Nox1916@users.noreply.github.com>
2023-11-13 22:37:39 +0530

    Create sponsors.js

---
+++ script/sponsors.js
@@ -0,0 +1,14 @@
+(function() {
+  var slider = document.querySelector('.sponsors .slider');
+  var images = slider.querySelectorAll('img');
+
+  var currentImage = 0;
+
+  function slide() {
+    images[currentImage].classList.remove('active');
+    currentImage = (currentImage + 1) % images.length;
+    images[currentImage].classList.add('active');
+  }
+
+  setInterval(slide, 3000);
+})();
